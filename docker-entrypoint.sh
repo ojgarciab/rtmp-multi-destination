e173f8c61f424e8e27d932738a2d9ff88ebfdf1d
Oscar Garcia <oscar.github@linaresdigital.com>
2020-11-03 14:46:52 +0100

    Added Twitch support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,23 @@ if [ -n "$RTMP_YOUTUBE_URLS" ]; then
     for URL in ${RTMP_YOUTUBE_URLS}; do
         # If it is NOT a rtmp scheme, only a key, then add default base URL to it
         if [ "${URL%%:*}" != "rtmp" ]; then
-            URL="rtmp://a.rtmp.youtube.com/live2/$URL"
+            URL="${RTMP_YOUTUBE_BASE}${URL}"
 	fi
         # Output one "push" per Youtube URL
         echo "push ${URL};" >> /etc/nginx/rtmp.d/youtube.conf
+    done
+fi
+
+# Check if Twitch URLS are present
+if [ -n "$RTMP_TWITCH_URLS" ]; then
+    # Load them one by one
+    for URL in ${RTMP_TWITCH_URLS}; do
+        # If it is NOT a rtmp scheme, only a key, then add default base URL to it
+        if [ "${URL%%:*}" != "rtmp" ]; then
+            URL="${RTMP_TWITCH_BASE}${URL}"
+	fi
+        # Output one "push" per Twitch URL
+        echo "push ${URL};" >> /etc/nginx/rtmp.d/twitch.conf
     done
 fi
 
